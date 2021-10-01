d340cd79e42461866b43d1d1f6be072726d38d65
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2021-10-01 17:05:11 +0100

    Sprites now appear and disappear correctly. Next step is to add hitting and blocking.

--- Assets/Scripts/Choices.cs
+++ Assets/Scripts/Choices.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class Choices : MonoBehaviour
 {
+
+    // Sprites Player_1
+    [SerializeField] GameObject p1RockSprite = null;
+    [SerializeField] GameObject p1PaperSprite = null;
+    [SerializeField] GameObject p1ScissorsSprite = null;
+    // Sprite Player_2
+    [SerializeField] GameObject p2RockSprite = null;
+    [SerializeField] GameObject p2PaperSprite = null;
+    [SerializeField] GameObject p2ScissorsSprite = null;
+
+    // Round timer
+    public float roundTimer = 0.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        roundTimer = 2.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Start round timer
+        if (Shooting.startTimer == true) {
+            roundTimer -= Time.deltaTime;
+		}
+
+        if (roundTimer <= 0) {
+            // Reset all sprites to false ready for the next round
+            p1RockSprite.SetActive(false);
+            p1PaperSprite.SetActive(false);
+            p1ScissorsSprite.SetActive(false);
+            p2RockSprite.SetActive(false);
+            p2PaperSprite.SetActive(false);
+            p2ScissorsSprite.SetActive(false);
+            // Reset all the shooting flags (public static bools)
+            Shooting.p1RockPressed = false;
+            Shooting.p1PaperPressed = false;
+            Shooting.p1ScissorsPressed = false;
+            Shooting.p2RockPressed = false;
+            Shooting.p2PaperPressed = false;
+            Shooting.p2ScissorsPressed = false;
+            // Reset startTimer public bool 
+            Shooting.startTimer = false;
+            // Reset roundTimer
+            roundTimer = 2.0f;
+        }
+
+        // Turn on sprites for each players choice
+        if (Shooting.p1RockPressed == true) {
+            p1RockSprite.SetActive(true);
+		}
+
+        if (Shooting.p1PaperPressed == true) {
+            p1PaperSprite.SetActive(true);
+        }
+
+        if (Shooting.p1ScissorsPressed == true) {
+            p1ScissorsSprite.SetActive(true);
+        }
+
+        if (Shooting.p2RockPressed == true) {
+            p2RockSprite.SetActive(true);
+        }
+
+        if (Shooting.p2PaperPressed == true) {
+            p2PaperSprite.SetActive(true);
+        }
+
+        if (Shooting.p2ScissorsPressed == true) {
+            p2ScissorsSprite.SetActive(true);
+        }
+
+
     }
 }
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -7,6 +7,17 @@ public class Shooting : MonoBehaviour
     [SerializeField] private GameObject bullet = null;
     private Vector3 spawnLocation;
 
+    // Public Choices
+    public static bool p1RockPressed = false;
+    public static bool p1PaperPressed = false;
+    public static bool p1ScissorsPressed = false;
+
+    public static bool p2RockPressed = false;
+    public static bool p2PaperPressed = false;
+    public static bool p2ScissorsPressed = false;
+
+    public static bool startTimer = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +27,51 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if (Input.anyKeyDown) {
+        // Fire when any key is pressed
+        if (Input.anyKeyDown) {
+            // Start global timer
+            startTimer = true;
+            // Create an instance of the bullet
             Instantiate(bullet, spawnLocation , Quaternion.identity);
 	    }
-    }
 
-    
+        // - Player 1
+        // Left pressed (Rock)
+        if (Input.GetKeyDown("a")) {
+            Debug.Log("LeftArrow");
+            p1RockPressed = true;
+        }
+
+        // Up pressed (Paper)
+        if (Input.GetKeyDown("w")) {
+            Debug.Log("UpArrow");
+            p1PaperPressed = true;
+	    }
+
+        // Right pressed (Scissors)
+        if (Input.GetKeyDown("d")) {
+            Debug.Log("RightArrow");
+            p1ScissorsPressed = true;
+        }
+
+        // -Player 2
+        // Left pressed (Rock)
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            Debug.Log("LeftArrow");
+            p2RockPressed = true;
+        }
+
+        // Up pressed (Paper)
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            Debug.Log("UpArrow");
+            p2PaperPressed = true;
+        }
+
+        // Right pressed (Scissors)
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            Debug.Log("RightArrow");
+            p2ScissorsPressed = true;
+        }
+
+    }   
 }
